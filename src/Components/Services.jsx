c2024d31210aa3299f60ee466ffa9086a9ca927d
AnasCoding1986 <anassust.scw@gmail.com>
2024-07-08 20:28:29 +0600

    working with routing system

---
+++ src/Components/Services.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Services() {
+  return (
+    <div>
+      <h2>Different kind of services</h2>
+    </div>
+  )
+}
---
+++ src/app/service/page.jsx
@@ -0,0 +1,11 @@
+import Services from "@/Components/Services";
+
+
+export default function page() {
+  return (
+    <div>
+      <h1>This is services</h1>
+      <Services></Services>
+    </div>
+  )
+}
