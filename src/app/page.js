31126604777e4e4223717721aa60ff3aa151eae6
AnasCoding1986 <anassust.scw@gmail.com>
2024-07-08 19:45:01 +0600

    after some break

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Heading from "./Components/Heading";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello next hero
-      <button>Call me</button>
+      <Heading></Heading>
     </main>
   );
 }
