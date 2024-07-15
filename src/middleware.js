cec31591f987bbe2cdca471510193c853686a7f2
AnasCoding1986 <anassust.scw@gmail.com>
2024-07-15 11:23:30 +0600

    middlewire done

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,20 @@
 import { NextResponse } from "next/server"
 
+
+// const user = true;
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    if(request.nextUrl.pathname.startsWith('/about')){
-        return NextResponse.redirect(new URL('/blogs', request.url))
+    const cookies = request.cookies.get('token');
+    if(!cookies){
+        return NextResponse.redirect(new URL('.login', request.url))
     }
+
+    return NextResponse.next();
   
 }
  
 // See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: ['/about', '/service'],
-// }
+export const config = {
+  matcher: ['/dashBoard', '/service'],
+}
