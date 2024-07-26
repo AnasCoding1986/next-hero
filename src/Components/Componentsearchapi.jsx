3eebccdff895dd79a714c143b4adebee5967dc19
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-07-26 23:16:54 +0600

    continuation commit

--- src/Components/Componentsearchapi.jsx
+++ src/Components/Componentsearchapi.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 
-const searchapi = () => {
+const Componentssearchapi = () => {
+
+    const [search,setSearch] = useState('abong');
+    const [posts,setpost] = useState('abong');
+
+    const loadData = async() => {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        const data = res.json();
+        setpost(data);
+    }
+
+    const handler = (e) => {
+        setSearch(e.target.value);
+        console.log(e.target.value);
+    }
+
+    // useEffect(()=>{
+    //     loadData();
+    // },[search])
+
     return (
         <div>
-            <input type="text" placeholder='Search' className='p-2 border-none outline-none text-white bg-slate-400' />
+            <input onChange={handler} type="text" placeholder='Search' className='p-2 border-none outline-none text-white bg-slate-400' />
             <button className='bg-red-200 p-2'>Search</button>
+            <div className='mt-12 grid grid-cols-3 gap-3'>
+                {
+                    posts?.map((post)=> (
+                        <div key={post.id} className='border-2 p-3' >
+                            <h2>title: {post.title}</h2>
+                            <p>body: {post.body}</p>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
 
-export default searchapi;
+export default Componentssearchapi;
--- src/app/SeaarchApi/page.jsx
+++ src/app/SeaarchApi/page.jsx
@@ -1,10 +1,13 @@
+import Componentssearchapi from '@/Components/Componentsearchapi';
 import React from 'react';
 
 const SearchApi = () => {
+
     return (
         <div>
             <h1>Search by Id</h1>
             <p>Input your desired id</p>
+            <Componentssearchapi></Componentssearchapi>
         </div>
     );
 };
