4a0fd0f67341050146342e5ef21cb1de592f0c35
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-07-30 11:45:07 +0600

    auth implemented

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    session: {
+        strategy: "jwt"
+    },
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                username: { label: "Email", type: "text", required: true, placeholder: "Your Name" },
+                password: { label: "Password", type: "password", required: true, placeholder: "Enter Password" }
+            },
+            async authorize(credentials) {
+                if (!credentials) {
+                    return null;
+                }
+                return true;
+            }
+        })
+    ]
+})
+
+export { handler as GET, handler as POST }
